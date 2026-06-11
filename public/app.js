5e505600b7c1acf7f6a1e0e0e1360b592e3d6a1b
pointfinauto-collab <pointfinauto@gmail.com>
2026-06-11 21:46:44 +0300

    Update app.js

--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
-const API_BASE = 'http://localhost:5000/api';
+const API_BASE = 'https://canadian-immigration-portal.onrender.com//api';
 
 function showAuthTab(target) {
     if(target === 'login') {
